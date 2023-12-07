ba19fde9dbca8159f5afd672651a73b74b3ee18a
Ismael Abreu <ismaelga@users.noreply.github.com>
2023-12-07 10:39:14 +0000

    day 6

--- lib/advent_of_code/solution/year_2023/day_06.ex
+++ lib/advent_of_code/solution/year_2023/day_06.ex
@@ -1,7 +1,38 @@
 defmodule AdventOfCode.Solution.Year2023.Day06 do
-  def part1(_input) do
+  def part1(input) do
+    races = parse_races(input, :split)
+
+    Enum.map(races, &better_records/1)
+    |> Enum.reduce(&Kernel.*/2)
   end
 
-  def part2(_input) do
+  def part2(input) do
+    races = parse_races(input, :join)
+
+    Enum.map(races, &better_records/1)
+    |> Enum.reduce(&Kernel.*/2)
   end
+
+  def better_records({time, distance}) do
+    1..(time - 1)
+    |> Enum.filter(fn t ->
+      (time - t) * t > distance
+    end)
+    |> Enum.count()
+  end
+
+  def parse_races(input, variance) do
+    ["Time:" <> times, "Distance:" <> distances] = String.split(input, "\n", trim: true)
+
+    times = parse_numbers(times, variance)
+    distances = parse_numbers(distances, variance)
+
+    Enum.zip(times, distances)
+  end
+
+  defp parse_numbers(i, :split),
+    do: String.split(i, " ", trim: true) |> Enum.map(&String.to_integer/1)
+
+  defp parse_numbers(i, :join),
+    do: String.split(i, " ", trim: true) |> Enum.join() |> String.to_integer() |> List.wrap()
 end
