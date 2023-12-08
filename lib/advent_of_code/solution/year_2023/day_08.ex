b61ce012b090eb025e490e41468169f10d60030a
Ismael Abreu <ismaelga@users.noreply.github.com>
2023-12-08 14:41:46 +0000

    day 8 part 2

--- lib/advent_of_code/solution/year_2023/day_08.ex
+++ lib/advent_of_code/solution/year_2023/day_08.ex
@@ -1,24 +1,58 @@
 defmodule AdventOfCode.Solution.Year2023.Day08 do
   def part1(input) do
+    {moves, parts} = parse(input)
+
+    Stream.cycle(moves)
+    |> Enum.reduce_while({"AAA", 0}, fn move, {current, count} ->
+      case {move, Map.get(parts, current)} do
+        {"R", {_, "ZZZ", _}} -> {:halt, count + 1}
+        {"L", {"ZZZ", _, _}} -> {:halt, count + 1}
+        {"R", {_, c, _}} -> {:cont, {c, count + 1}}
+        {"L", {c, _, _}} -> {:cont, {c, count + 1}}
+      end
+    end)
+  end
+
+  def part2(input) do
+    {moves, parts} = parse(input)
+
+    init =
+      parts
+      |> Enum.filter(fn {_, {_, _, l}} -> l == "A" end)
+      |> Enum.map(&elem(&1, 0))
+
+    init
+    |> Enum.map(fn init -> steps(init, moves, parts) end)
+    |> Enum.reduce(fn a, b -> trunc(a * b / Integer.gcd(a, b)) end)
+  end
+
+  def steps(start, moves, parts) do
+    Stream.cycle(moves)
+    |> Enum.reduce_while({start, 0}, fn move, {current, count} ->
+      case step(move, parts, current) do
+        {:halt, _} -> {:halt, count}
+        {:cont, c} -> {:cont, {c, count + 1}}
+      end
+    end)
+  end
+
+  def step(move, parts, current) do
+    case {move, Map.get(parts, current)} do
+      {"R", {_, c, "Z"}} -> {:halt, c}
+      {"L", {c, _, "Z"}} -> {:halt, c}
+      {"R", {_, c, _}} -> {:cont, c}
+      {"L", {c, _, _}} -> {:cont, c}
+    end
+  end
+
+  def parse(input) do
     [moves | parts] =
       input
       |> String.split("\n", trim: true)
 
     moves = moves |> String.split("", trim: true)
     parts = parse_parts(parts)
-
-    Stream.cycle(moves)
-    |> Enum.reduce_while({"AAA", 0}, fn move, {current, count} ->
-      case {move, Map.get(parts, current)} do
-        {"R", {_, "ZZZ"}} -> {:halt, count + 1}
-        {"L", {"ZZZ", _}} -> {:halt, count + 1}
-        {"R", {_, c}} -> {:cont, {c, count + 1}}
-        {"L", {c, _}} -> {:cont, {c, count + 1}}
-      end
-    end)
-  end
-
-  def part2(_input) do
+    {moves, parts}
   end
 
   def parse_parts(parts) do
@@ -29,7 +63,9 @@ defmodule AdventOfCode.Solution.Year2023.Day08 do
       [left, right] =
         leafs |> String.replace(["(", ")"], "") |> String.trim() |> String.split(", ", trim: true)
 
-      {node |> String.trim(), {left, right}}
+      node = node |> String.trim()
+      last = node |> String.graphemes() |> List.last()
+      {node, {left, right, last}}
     end)
     |> Enum.into(%{})
   end
